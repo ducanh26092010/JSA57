2ace4a701468d90199716cafea766278e691eaf1
ducanh26092010 <bin26092010@gmail.com>
2025-09-03 21:00:55 +0700

    BTVN

---
+++ BTVN/Buoi 2/Bai 1/js/bai1.js
@@ -0,0 +1,15 @@
+var city = "Ha Noi";
+
+let age = 25;
+
+
+const PI = 3.14;
+
+var temperature = 30;
+var greeting = "Hello world";
+var isJavaScriptFun = true;
+var nothing = null;
+var notDefined = underfined;
+const uniqueID = 1;
+const numbers = [123,45.7,45];
+const person = {id: 755, firstName: "ABC", lastName: "abc", birthDate: Date.now()};
---
+++ BTVN/Buoi 2/Bai 1/js/bai2.js
@@ -0,0 +1,31 @@
+// Khai báo một biến String và thực hiện các thao tác như nối chuỗi, cắt chuỗi (substring), 
+// và tìm độ dài của chuỗi.
+// khai bao
+let fullName = "ABC";
+// noi chuoi
+fullName += " MNO";
+// substring
+const firstName = fullName.slice(0, 3);
+console.log(fullName);
+console.log(firstName);
+console.log(fullName.substring(0, 3));
+
+// Nhập vào một chuỗi từ người dùng, sau đó in ra chuỗi đó đã được in hoa.
+// input
+const input = prompt("Name: ");
+alert(input.trim()? input.toUpperCase() : "Chua dien ten");
+
+// Khai báo một biến chứa tên người dùng (String) và tuổi (Number), 
+// sau đó in ra thông báo dạng: "Tên bạn là X và bạn Y tuổi."
+let name = "Dang Hoang Long";
+let age1 = 14;
+const result = `Ten ban la ${name} va ban ${age1} tuoi`;
+console.log(result);
+
+
+// Tạo một chương trình nhận vào một câu từ người dùng, 
+// đếm số ký tự trong câu và in ra độ dài của câu đó.
+const sentence = prompt("Viet cau");
+console.log("Do dai cau", sentence.length);
+console.log("Tu", sentence.split(" ").length);
+// Sử dụng hàm prompt("…”); cho phép người dùng nhập dữ liệu từ bàn phím.
